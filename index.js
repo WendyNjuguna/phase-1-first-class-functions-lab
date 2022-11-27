5aea600c110f06df0b79dea1367fcad5575d0d28
Wendy <wendynjuguna714@gmail.com>
2022-11-27 22:48:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
-let drivers =['Antonia', 'Nuru','Amari', 'Mo']
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
 const returnFirstTwoDrivers = function(){
-    return drivers.slice(0,2);
+ return drivers.slice(0, 2)
 }
-  console.log(returnFirstTwoDrivers());
-
-  const returnLastTwoDrivers = function(){
-    return drivers.slice(-2);
-  }
-  console.log(returnLastTwoDrivers());
-
-  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-console.log(selectingDrivers[0])
-
-function createFareMultiplier(num){
-    return function(fare){
-        return fare * num;
-    }
+const returnLastTwoDrivers= function (){
+    return drivers.slice( 2);
 }
-const fareMultiplier = createFareMultiplier(4);
-
+const selectingDrivers= [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(fareValue){
+    return function (fare){
+        return fareValue * fare;
+    };
+}
 const fareDoubler = createFareMultiplier(2);
-const fareTripler = createFareMultiplier(3);
-
-function selectDifferentDrivers(drivers, cb){
-    if(cb === returnFirstTwoDrivers){
-        return returnFirstTwoDrivers(drivers);
-    }else if(cb === returnLastTwoDrivers){
-        return returnLastTwoDrivers(drivers)
-    }}
+const fareTripler =  createFareMultiplier(3);
+function selectDifferentDrivers(drivers, selectingDrivers){
+        return selectingDrivers(drivers)
+    }
